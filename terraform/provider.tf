79ee82654d95fa856ce622fb71e67e7ef1585185
Waqas Khalique <waqcas@gmail.com>
2026-04-25 20:57:23 +0500

    s3 bucket updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,14 @@
 provider "aws" {
     region = "ap-south-1"
-    # credentials removed
+    
 }
+
+terraform {
+  backend "s3" {
+    bucket = "amzn-waqcas-s3-bucket"
+    key    = "path/to/my/key"
+    region = "ap-south-1"
+  }
+}
+
 
